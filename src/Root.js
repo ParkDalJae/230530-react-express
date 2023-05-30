f866e569daa0a2284f238d99f298a1f60e026a4c
ParkDalJae <dv.over931@gmail.com>
2023-05-30 15:25:53 +0900

    싹 다 밈

--- src/Root.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Root = () => {
-  return <h3> hello react!</h3>;
-};
-
-export default Root;
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import Root from "./Root";
-
-ReactDom.render(<root />, document.getElementById("root"));
